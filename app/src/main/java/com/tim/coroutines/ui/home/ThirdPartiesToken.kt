96b69e6495060de95072c2d5a28e853f1cdd19b6
HsiaoTing Chen <fatfattim@gmail.com>
2023-09-19 14:36:04 +0800

    test

--- app/src/main/java/com/tim/coroutines/ui/home/ThirdPartiesToken.kt
+++ app/src/main/java/com/tim/coroutines/ui/home/ThirdPartiesToken.kt
@@ -7,7 +7,9 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -15,13 +17,13 @@ class ThirdPartiesToken {
     // The Job represents the current state of the coroutine, and Dispatchers.IO specifies that the coroutines launched within this scope will run on the IO thread.
     private val scope = CoroutineScope(Job() + Dispatchers.IO)
     // Create a MutableStateFlow to emit the platform information
-    private val platformFlow = MutableStateFlow(SocialMedia.NONE)
+    private val platformFlow = MutableSharedFlow<SocialMedia>(0)
 
     // Expose the platformFlow as a Flow for external subscribers
-    fun platformUpdates(): Flow<SocialMedia> = platformFlow.asStateFlow()
+    fun platformUpdates(): Flow<SocialMedia> = platformFlow.asSharedFlow()
     fun fetchToken(platform: SocialMedia) {
         scope.launch {
-            platformFlow.value = fetchTokenFrom(platform)
+            platformFlow.emit(fetchTokenFrom(platform))
         }
     }
 
